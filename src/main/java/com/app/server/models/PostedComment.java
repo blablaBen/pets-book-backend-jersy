df76a726ac3246c5bf6a467495447be017e185c8
Pattarasai Markpeng <pattarasaimarkpeng@Pattarasais-MacBook-Pro.local>
2018-10-15 16:03:21 -0700

    add PostedComment/PostedStatus

---
+++ src/main/java/com/app/server/models/PostedComment.java
@@ -0,0 +1,57 @@
+package com.app.server.models;
+
+public class PostedComment {
+    public String getId() {
+        return id;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    String id=null;
+    String postId;
+    String content;
+    String time;
+    String userId;
+
+
+    public PostedComment(String postId, String content, String time, String userId) {
+        this.postId = postId;
+        this.content = content;
+        this.time = time;
+        this.userId = userId;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/app/server/models/PostedStatus.java
@@ -0,0 +1,69 @@
+package com.app.server.models;
+
+import java.util.List;
+
+public class PostedStatus {
+    public String getId() {
+        return id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getTextValue() {
+        return textValue;
+    }
+
+    public void setTextValue(String textValue) {
+        this.textValue = textValue;
+    }
+
+    public List<String> getPictures() {
+        return pictures;
+    }
+
+    public void setPictures(List<String> pictures) {
+        this.pictures = pictures;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getCommentCount() {
+        return commentCount;
+    }
+
+    public void setCommentCount(int commentCount) {
+        this.commentCount = commentCount;
+    }
+
+    String id=null;
+    String userId;
+    String textValue;
+    List<String> pictures;
+    String date;
+    int commentCount;
+
+
+    public PostedStatus(String userId, String textValue, List<String> pictures, String date, int commentCount) {
+        this.userId = userId;
+        this.textValue = textValue;
+        this.pictures = pictures;
+        this.date = date;
+        this.commentCount = commentCount;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
