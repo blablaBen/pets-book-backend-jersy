23cf18d951bb4f62ea81f82d9bac9962ca713c78
Pattarasai Markpeng <pattarasaimarkpeng@Pattarasais-MacBook-Pro.local>
2018-11-04 20:32:04 -0800

    add notification

---
+++ src/main/java/com/app/server/models/Notification.java
@@ -0,0 +1,9 @@
+package com.app.server.models;
+
+public class Notification {
+    private String id = null;
+    private String userId;
+    private String type;
+    private String content;
+    private boolean isRead;
+}
